a0c1881b99c1c5e3a18891ea374edf28e205bf47
Luis Lopez <llopez1518@gmail.com>
2021-07-25 10:43:09 -0600

    add connection file4

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const db = require('knex')(connection);
 
 app.set('db', db)
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server is listening on port 5000...");
   });
